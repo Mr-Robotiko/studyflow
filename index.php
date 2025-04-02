71de3b3b8f560b94085f97f1a4810f65e301cdc5
Raphael Tack <raphael.tack@partner.ibm.com>
2025-04-02 16:27:33 +0200

    add form and delete other files -> dynamic design

---
+++ index.php
@@ -0,0 +1,10 @@
+<form action="#" method="post">
+        <label for="username">Benutzername:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="text" id="password" name="password" required><br><br>
+
+        <button type="submit">Absenden</button>
+        <button type="reset">Zurücksetzen</button>
+</form>
