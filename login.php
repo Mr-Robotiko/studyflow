7cefa0b01eb61724f1d3910cfad6ce21ee89f257
Claas Wenk <claas.wenk@gmail.com>
2025-04-09 16:20:12 +0200

    neu

--- login.php
+++ login.php
@@ -8,13 +8,13 @@
     <h1>Welcome to StudyFlow</h1>
 
     <form action="#" method="post">
-          <label for="username">Benutzername:</label>
+          <label for="username">Benutzername</label>
           <input type="text" id="username" name="username" required><br><br>
 
-          <label for="password">Password:</label>
+          <label for="password">Passwort</label>
           <input type="text" id="password" name="password" required><br><br>
 
-          <button type="submit">Absenden</button>
+          <button type="submit">Login</button>
           <button type="reset">Zurücksetzen</button>
     </form>
   </body>
