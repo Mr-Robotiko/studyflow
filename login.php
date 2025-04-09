1cdf7ce0959f464c35d93814c6ea9830fcec4062
Claas Wenk <claas.wenk@gmail.com>
2025-04-09 15:37:30 +0200

    index.php --> index.login geändert

---
+++ login.php
@@ -0,0 +1,16 @@
+<head>
+        <meta charset="UTF8">
+        <title>Stucal Login</title>
+        <meta name="Studycal Login" content="Studycal Login">
+</head>
+
+<form action="#" method="post">
+        <label for="username">Benutzername:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="text" id="password" name="password" required><br><br>
+
+        <button type="submit">Absenden</button>
+        <button type="reset">Zurücksetzen</button>
+</form>
