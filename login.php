42b74797dc3529afb7dbaae64cfc39d76e062dc4
Claas Wenk <claas.wenk@gmail.com>
2025-04-09 16:14:33 +0200

    Neues Feature

--- login.php
+++ login.php
@@ -1,18 +1,21 @@
+<html lang="de">
+  <head>
+    <meta charset="UTF8">
+    <title>Stucal Login</title>
+    <meta name="Studycal Login" content="Studycal Login">
+  </head>
+  <body>
+    <h1>Welcome to StudyFlow</h1>
 
-<head>
-        <meta charset="UTF8">
-        <title>Stucal Login</title>
-        <meta name="Studycal Login" content="Studycal Login">
-</head>
-<h1>Welcome to StudyFlow</h1>
+    <form action="#" method="post">
+          <label for="username">Benutzername:</label>
+          <input type="text" id="username" name="username" required><br><br>
 
-<form action="#" method="post">
-        <label for="username">Benutzername:</label>
-        <input type="text" id="username" name="username" required><br><br>
+          <label for="password">Password:</label>
+          <input type="text" id="password" name="password" required><br><br>
 
-        <label for="password">Password:</label>
-        <input type="text" id="password" name="password" required><br><br>
-
-        <button type="submit">Absenden</button>
-        <button type="reset">Zurücksetzen</button>
-</form>
+          <button type="submit">Absenden</button>
+          <button type="reset">Zurücksetzen</button>
+    </form>
+  </body>
+</html>
