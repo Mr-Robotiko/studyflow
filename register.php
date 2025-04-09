884fb63bf52595317c30ecd048dd5aa5dbc0f64e
Claas Wenk <claas.wenk@gmail.com>
2025-04-09 16:23:42 +0200

    Registrierungs-Seite

---
+++ register.php
@@ -0,0 +1,27 @@
+<html lang="de">
+  <head>
+    <meta charset="UTF8">
+    <title>Registrierung</title>
+    <meta name="Studycal Login" content="Studycal Login">
+  </head>
+  <body>
+    <h1>Willkommen bei StudyCal</h1>
+
+    <form action="#" method="post">
+          <label for="username">Benutzername</label>
+          <input type="text" id="username" name="username" required><br><br>
+
+          <label for="password">Passwort</label>
+          <input type="text" id="password" name="password" required><br><br>
+
+          <label for="password">Passwort wiederholen</label>
+          <input type="text" id="passwordwdh" name="passwordwdh" required><br><br>
+
+          <label for="password">Sicherheitsfrage</label>
+          <input type="text" id="securityquest" name="securityquest" required><br><br>
+
+          <button type="submit">Registrieren</button>
+          <button type="reset">Zurücksetzen</button>
+    </form>
+  </body>
+</html>
