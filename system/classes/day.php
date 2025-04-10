1928e65abc0c2aa4975a1991e8d957eabda53ee4
Raphael Tack <raphael.tack@partner.ibm.com>
2025-04-10 09:03:09 +0200

    add day class

---
+++ system/classes/day.php
@@ -0,0 +1,31 @@
+<?php
+    class Day {
+        private $day;
+        private $Task;
+
+        public function __construct($day = NULL, $Task = NULL) {
+            $this->setDay($day);
+            $this->setTask($Task);
+        }
+
+        public function getDay() {
+            return $this->day;
+        }
+
+        public function getTask() {
+            return $this->Task;
+        }
+
+        public function setDay($dayValue) {
+            $this->day = $dayValue;
+        }
+
+        public function setTask($TaskValue) {
+            $this->Task = $TaskValue;
+        }
+
+        public function __toString() {
+            return "Day: {$this->day}\nTask: {$this->Task}";
+        }
+    }
+?>
