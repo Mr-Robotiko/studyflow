ea5b8853757269d1352ebefdf948621be2c95aa9
Claas Wenk <claas.wenk@gmail.com>
2025-04-30 15:35:08 +0200

    register.js neu

--- register.php
+++ register.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>StudyCal - Registrierung</title>
+  <script type="text/javascript" src="system/javascript/login.js"></script>
   <link rel="stylesheet" href="/system/style/main.css"/>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
@@ -50,7 +51,7 @@
 
       <!-- Button -->
       <div class="buttons" style="justify-content: center;">
-        <button type="submit" class="btn">Registrieren</button>
+        <button onclick="input_to_var()" type="submit" class="btn">Registrieren</button>
       </div>
     </form>
   </div>
