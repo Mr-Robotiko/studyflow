77aaa6a140a6feb78d57e8e3ab107084466395d4
Claas Wenk <claas.wenk@gmail.com>
2025-05-22 08:14:17 +0200

    Settings.php Session removed

--- settings.php
+++ settings.php
@@ -1,42 +1,42 @@
 <?php
-session_start();
+//session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-require_once "system/user-classes/user.php";
+// require_once "system/user-classes/user.php";
 
-define('SESSION_TIMEOUT', 600); // Timeout auf 10 Minuten setzen
+// define('SESSION_TIMEOUT', 600); // Timeout auf 10 Minuten setzen
 
-// Benutzer muss eingeloggt sein
-if (!isset($_SESSION["eingeloggt"]) || !$_SESSION["eingeloggt"]) {
-    header("Location: login.php");
-    exit;
-}
+// // Benutzer muss eingeloggt sein
+// if (!isset($_SESSION["eingeloggt"]) || !$_SESSION["eingeloggt"]) {
+//     header("Location: login.php");
+//     exit;
+// }
 
-// Session-Timeout prüfen (nur wenn 'LAST_ACTIVITY' gesetzt ist)
-if (isset($_SESSION['LAST_ACTIVITY'])) {
-    if (time() - $_SESSION['LAST_ACTIVITY'] > SESSION_TIMEOUT) {
-        // Session ist abgelaufen, Benutzer wird zur Startseite (index.html) weitergeleitet
-        session_unset();
-        session_destroy();
-        header("Location: index.html"); // Umleitung zur Startseite
-        exit;
-    }
-}
+// // Session-Timeout prüfen (nur wenn 'LAST_ACTIVITY' gesetzt ist)
+// if (isset($_SESSION['LAST_ACTIVITY'])) {
+//     if (time() - $_SESSION['LAST_ACTIVITY'] > SESSION_TIMEOUT) {
+//         // Session ist abgelaufen, Benutzer wird zur Startseite (index.html) weitergeleitet
+//         session_unset();
+//         session_destroy();
+//         header("Location: index.html"); // Umleitung zur Startseite
+//         exit;
+//     }
+// }
 
-// Setze die letzte Aktivität auf die aktuelle Zeit
-$_SESSION['LAST_ACTIVITY'] = time();
+// // Setze die letzte Aktivität auf die aktuelle Zeit
+// $_SESSION['LAST_ACTIVITY'] = time();
 
-// User aus Session-Daten neu erstellen
-$data = $_SESSION['user_data'];
-$user = new User();
-$user->setUserName($data['username']);
-$user->setName($data['name']);
-$user->setSurname($data['surname']);
-$user->setSecurityPassphrase($data['securityPassphrase']);
-$user->setCalendarfile($data['calendarfile'] ?? null);
+// // User aus Session-Daten neu erstellen
+// $data = $_SESSION['user_data'];
+// $user = new User();
+// $user->setUserName($data['username']);
+// $user->setName($data['name']);
+// $user->setSurname($data['surname']);
+// $user->setSecurityPassphrase($data['securityPassphrase']);
+// $user->setCalendarfile($data['calendarfile'] ?? null);
 ?>
 
 <!DOCTYPE html>
