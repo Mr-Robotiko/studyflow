37270239f8b90f1226575c3ff5e334804ea7fbc5
Raphael Tack <raphael.tack@partner.ibm.com>
2025-05-29 11:15:46 +0200

    add settings saver

--- system/user-classes/account-manager.php
+++ system/user-classes/account-manager.php
@@ -2,9 +2,11 @@
 
 class AccountManager {
     private User $user;
+    private PDO $conn;
 
-    public function __construct(User $user) {
+    public function __construct(User $user, PDO $conn) {
         $this->user = $user;
+        $this->conn = $conn;
     }
 
     public function deleteAccount(): bool {
@@ -14,4 +16,15 @@ class AccountManager {
             throw new Exception("Fehler beim Löschen: " . $e->getMessage());
         }
     }
+
+    public function updateUserSettings(int $mode, int $ILT): bool {
+        $stmt = $this->conn->prepare("UPDATE user SET Mode = :mode, ILT = :ilt WHERE Username = :username");
+        return $stmt->execute([
+            ':mode'     => $mode,
+            ':ilt'      => $ILT,
+            ':username' => $this->user->getUserName()
+        ]);
+    }
 }
+
+?>
