d16b8bd2da0b2ba0de5a7abe98d32f9d09ce706e
Raphael Tack <raphael-tack@t-online.de>
2025-06-28 13:41:12 +0200

    login.php überarbeitet

--- login.php
+++ login.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once "system/user-classes/user.php";
 require_once "system/database-classes/database.php";
 require_once "system/login-classes/login-class.php";
